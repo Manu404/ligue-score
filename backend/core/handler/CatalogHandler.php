b7a8fc444dd1f427aef56bb17f512498aab5052a
Emmanuel Istace <istace.emmanuel@hotmail.com>
2021-06-30 02:44:50 +0200

    implement complete catalo retreiveing in a single call

--- backend/core/handler/CatalogHandler.php
+++ backend/core/handler/CatalogHandler.php
@@ -26,58 +26,91 @@ class CatalogHandler extends HandlerBase {
         $this->InitializeGet();
     }
 
+    private function GetDays(){
+        $query = GamedayQuery::create();
+        $queryResults = $query->find();
+        $result = array();
+        foreach($queryResults as $queryResult) {
+            array_push($result, ToJSON::Gameday($queryResult));
+        }    
+        return $result;
+    }
+    private function GetTypes(){
+        $query = GametypeQuery::create();
+        $queryResults = $query->find();
+        $result = array();
+        foreach($queryResults as $queryResult) {
+            array_push($result, ToJSON::Gametype($queryResult));
+        }     
+        return $result;
+    }
+    private function GetPlayers(){
+        $playerQuery = PlayerQuery::create();
+        $players = $playerQuery->find();
+        $result = array();
+        foreach($players as $player) {
+            array_push($result, ToJSON::Player($player));
+        }     
+        return $result;
+    }
+    private function GetRules(){
+        $query = RulesQuery::create();
+        $queryResults = $query->find();
+        $result = array();
+        foreach($queryResults as $queryResult) {
+            array_push($result, ToJSON::Rules($queryResult));
+        }   
+        return $result;
+    }
+    private function GetShopItems(){
+        $query = ShopitemsQuery::create();
+        $queryResults = $query->find();
+        $result = array();
+        foreach($queryResults as $queryResult) {
+            array_push($result, ToJSON::ShopItem($queryResult));
+        }       
+        return $result;
+    }
+
     private function InitializeGet() {
         $this->app->get('/api/v1/catalog/day/', function (Request $request, Response $response, array $args) {  
-            $query = GamedayQuery::create();
-            $queryResults = $query->find();
-            $result = array();
-            foreach($queryResults as $queryResult) {
-                array_push($result, ToJSON::Gameday($queryResult));
-            }                  
-            return HandlerBase::PrepareGetResponse($result, $response);     
+              
+            return HandlerBase::PrepareGetResponse($this->GetDays(), $response);     
         });
 
         $this->app->get('/api/v1/catalog/type/', function (Request $request, Response $response, array $args) {  
-            $query = GametypeQuery::create();
-            $queryResults = $query->find();
-            $result = array();
-            foreach($queryResults as $queryResult) {
-                array_push($result, ToJSON::Gametype($queryResult));
-            }                  
-            return HandlerBase::PrepareGetResponse($result, $response);     
+             
+            return HandlerBase::PrepareGetResponse($this->GetTypes(), $response);     
         });
 
         $this->app->get('/api/v1/catalog/player/{id:.*}', function (Request $request, Response $response, array $args) {  
             if($args["id"] != null && is_numeric($args["id"])) {
             }
             else {
-                $playerQuery = PlayerQuery::create();
-                $players = $playerQuery->find();
-                $result = array();
-                foreach($players as $player) {
-                    array_push($result, ToJSON::Player($player));
-                }                  
-                return HandlerBase::PrepareGetResponse($result, $response);
+             
+                return HandlerBase::PrepareGetResponse($this->GetPlayers(), $response);
             }         
         });
 
         $this->app->get('/api/v1/catalog/rule/', function (Request $request, Response $response, array $args) {  
-            $query = RulesQuery::create();
-            $queryResults = $query->find();
-            $result = array();
-            foreach($queryResults as $queryResult) {
-                array_push($result, ToJSON::Rules($queryResult));
-            }                  
-            return HandlerBase::PrepareGetResponse($result, $response);     
+               
+            return HandlerBase::PrepareGetResponse($this->GetRules(), $response);     
         });
 
         $this->app->get('/api/v1/catalog/shop/', function (Request $request, Response $response, array $args) {  
-            $query = ShopitemsQuery::create();
-            $queryResults = $query->find();
-            $result = array();
-            foreach($queryResults as $queryResult) {
-                array_push($result, ToJSON::ShopItem($queryResult));
-            }                  
+             
+            return HandlerBase::PrepareGetResponse($this->GetShopItems(), $response);     
+        });
+
+
+        $this->app->get('/api/v1/catalog/', function (Request $request, Response $response, array $args) {  
+             
+            $result = array(
+            "days" => $this->GetDays(),
+            "types" => $this->GetTypes(),
+            "players" => $this->GetPlayers(),
+            "rules" => $this->GetRules(),
+            "items" => $this->GetShopItems());
             return HandlerBase::PrepareGetResponse($result, $response);     
         });
     }
